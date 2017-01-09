ba8171a5ef7076d55788eeecedd1a84e8d4c3e11
TDozat <tdozat@gmail.com>
2017-01-09 14:15:34 -0800

    Added an experimental parser

--- lib/models/parsers/__init__.py
+++ lib/models/parsers/__init__.py
@@ -3,3 +3,8 @@ from stupid_parser import StupidParser
 from diag_parser import DiagParser
 from notag_parser import NoTagParser
 from kg_parser import KGParser
+# Experimental stuff
+try:
+  from parser2 import Parser2
+except ImportError:
+  pass
